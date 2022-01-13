9b56a689cf877e236a9ac7d4afc48f7277abb816
Peter Washington <peter@wmkagency.com>
2022-01-13 08:45:19 -0700

    init package setup

---
+++ src/classes/Img.ts
@@ -0,0 +1,69 @@
+import get from "lodash/get";
+// import { NodeImageFields } from "../fragments/nodeImageFields";
+
+export interface GatsbyImageData {
+  images: {
+    sources: { srcSet: string; sizes: string; type: string }[];
+    fallback: { srcSet: string; sizes: string; src: string };
+    width: number;
+    height: number;
+    layout: "fixed" | "fullWidth" | "constrained";
+  };
+  width: number;
+  height: number;
+  layout: "fixed" | "fullWidth" | "constrained";
+}
+
+/**
+ * @class
+ * Helper class for image queries that will
+ * hold data for Static/Dynamic images
+ * (via Gatsby)
+ */
+export class Img {
+  /**
+   * @param {Object} node - result of an image node query from Contentful
+   */
+  description: string;
+  title: string;
+  alt: string;
+  src: string | undefined;
+  private gatsby: GatsbyImageData;
+  private crops: { [key: string]: GatsbyImageData };
+  contentType: string;
+  constructor(node: object) {
+    const gatsby: GatsbyImageData = get(node, `gatsbyImageData`);
+    const _url: string = get(
+      node,
+      `file.url`,
+      get(
+        node,
+        `node.url`,
+        get(node, `gatsbyImageData.images.fallback.src`, "")
+      )
+    );
+    const url = _url.indexOf("//") === 0 ? "https:" + _url : _url;
+    this.title = get(node, `title`, get(node, `node.title`));
+    this.description = get(node, `description`, ``);
+    this.alt = get(node, `title`, get(node, `node.altText`, ""));
+    this.src = url;
+    this.gatsby = gatsby;
+    this.contentType = get(node, `file.contentType`);
+    this.crops = {};
+  }
+  addCrop(name: string, image: GatsbyImageData, log = false) {
+    const crops = this.crops;
+    crops[name] = image;
+    if (log) {
+      console.log(crops);
+    }
+  }
+  private crop(name: string) {
+    const cropped: GatsbyImageData = get(this, `crops.${name}`);
+    return cropped;
+  }
+  get(crop: string = "") {
+    const img = this.crop(crop);
+    return crop === "" ? this.gatsby : img; //Object.keys(img).map((k) => img[k])[0];
+  }
+}
---
+++ src/util/WmkImage.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { GatsbyImage } from "gatsby-plugin-image";
+import get from "lodash/get";
+import { Img } from "../classes/Img";
+/**
+ * @returns An image element or GatsbyImage depending on image data
+ */
+
+type imageProps = {
+  image: Img;
+  className?: string;
+  style?: object;
+  crop?: string;
+};
+
+export const WmkImage = ({
+  image,
+  className,
+  style,
+  crop = "",
+}: imageProps) => {
+  const gatsby = image.get(crop);
+  const isSvg = !!get(image, `contentType`, get(image, `src`, "")).match(/svg/);
+  return isSvg ? (
+    <img className={className} style={style} src={image.src} alt={image.alt} />
+  ) : gatsby ? (
+    <GatsbyImage
+      className={className}
+      style={style}
+      image={gatsby}
+      alt={image.alt}
+    />
+  ) : null;
+};
